3c7f9b4e2fff0f513482b174787cbbae1ecd3eff
fungos <fungos@gmail.com>
2010-11-02 14:40:15 -0200

    Installer

--- particle-viewer/mygame.cpp
+++ particle-viewer/mygame.cpp
@@ -17,9 +17,6 @@ const char *table[] = {
 	"psi/particle9.psi"
 };
 
-ResourceManager glMyResources("mine");
-
-
 MyGame::MyGame()
 	: bFollow(FALSE)
 {
@@ -37,7 +34,7 @@ MyGame::~MyGame()
 
 void MyGame::Setup(int argc, char **argv)
 {
-	pFileSystem->SetWorkDirectory("");
+	pFileSystem->SetWorkDirectory(L"");
 }
 
 BOOL MyGame::Initialize()
@@ -54,7 +51,7 @@ BOOL MyGame::Initialize()
 	pSceneManager->Add(&cScene);
 	/* ------- Rendering Initialization ------- */
 
-	sptLogo.Load(SPT_BG, &glMyResources);
+	sptLogo.Load(SPT_BG);
 	sptLogo.SetPosition(0.0f, 0.0f);
 	sptLogo.SetVisible(TRUE);
 	sptLogo.GetTexture()->SetFilter(Seed::TextureFilterTypeMin, Seed::TextureFilterNearest);
@@ -66,7 +63,7 @@ BOOL MyGame::Initialize()
 	glStringPool.OverRunReport();
 	glStringPool.PrintSnapshot();
 
-	fntFont.Load(FNT_FONT, &glMyResources);
+	fntFont.Load(FNT_FONT);
 	fntFont.SetFilter(Seed::TextureFilterTypeMin, Seed::TextureFilterNearest);
 	fntFont.SetFilter(Seed::TextureFilterTypeMag, Seed::TextureFilterNearest);
 	lblPos.SetFont(&fntFont);
@@ -75,7 +72,7 @@ BOOL MyGame::Initialize()
 	pGuiManager->Add(&lblPos);
 	cScene.Add(&lblPos);
 
-	cEmitter.Load(table[0], &glMyResources);
+	cEmitter.Load(table[0]);
 	cEmitter.SetSprite(SPT_PARTICLE);
 	cEmitter.SetPriority(10);
 	cEmitter.SetPosition(0.5f, 0.5f);
@@ -114,9 +111,9 @@ void MyGame::OnInputKeyboardRelease(const EventInputKeyboard *ev)
 		if (v >= 0 && v < 9)
 		{
 			cEmitter.Unload();
-			cEmitter.Load(table[v], &glMyResources);
+			cEmitter.Load(table[v]);
 			cEmitter.SetSprite(SPT_PARTICLE);
-			cEmitter.Play();	
+			cEmitter.Play();
 		}
 	}
 	else
