dea225efd4c4b5568a2938b60909deeccc8a1572
fungos <fungos@gmail.com>
2011-03-31 23:10:21 -0300

    typo

--- installer/linux.sh
+++ installer/linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# 1. Test if the is git, libogg, libvorbis, liboggfile, liboggplay, SDL, OpenAL, OpenGL, ...
+# 1. Test if there is git, libogg, libvorbis, liboggfile, liboggplay, SDL, OpenAL, OpenGL, ...
 # 2. clone SDK repository
 # 3. setup $SEEDSDK
 # 4. build
